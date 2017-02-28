e676a8ba0c1301fa7b6dc7dffe9476935b0b5bc6
krishnappa.halemani <krishnappa.halemani@xceedance.com>
2017-02-28 20:06:18 +0530

    Assembly name changed

--- BLAndDAL/BHSIEntities.Designer.cs
+++ BLAndDAL/BHSIEntities.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'c:\users\krishna\documents\visual studio 2015\Projects\AccordianSolution\BusinessLogic\BHSIEntities.edmx'. 
+﻿// T4 code generation is enabled for model 'G:\ACC\BHSI_Report\BLAndDAL\BHSIEntities.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
